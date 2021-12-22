3f11134ee0079f916a03b3c291f2cc2d15c85b04
Farnoud Taghdissi Borhani <v-ftaghd@microsoft.com>
2021-12-21 20:22:19 -0800

    Add project files.

---
+++ ConsoleApp2alternate high and low/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace alternate_high_and_low
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] A = { 9, 6, 8, 3, 7 };
+            Alternative(A);
+            Console.WriteLine(string.Join(",",A));
+        }
+
+        public static void Alternative(int[] A)
+        {
+            int len = A.Length;
+            for (int i = 1; i < A.Length; i+=2)
+            {
+                if (A[i-1] > A[i ])
+                    Swap(A, i, i - 1);
+
+                if (A[i + 1] < len && A[i + 1] > A[i])
+                    Swap(A, i, i + 1);
+            }
+        }
+
+        public static void Swap(int[] A, int X, int Y)
+        {
+            int tmp = A[X];
+            A[X] = A[Y];
+            A[Y] = tmp;
+        } 
+    }
+}
